297fcadc6167efb9c392e43009b7d4bb191320ca
naceurmossaab <naceurmossaab@gmail.com>
2021-10-10 22:03:11 +0100

    final version

--- data.js
+++ data.js
@@ -1,16 +1,19 @@
+//buy products
+var cart = [];
+var client = {};
+
+// iphones products 
 var iphoneProducts = [];
 var iphone13 = {};
 var iphone12 = {};
 var iphone11 = {};
 var iphone10 = {};
 
-window.iphoneProducts;
-
 iphone13.id = 0;
 iphone13.url = "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2021/09/apple-iphone-13-frandroid-2021.png";
 iphone13.name = "iphone13";
 iphone13.note = 9;
-iphone13.prix = 9;
+iphone13.prix = 859;
 iphone13.stock = 30;
 iphone13.details = "iphone 13 details";
 iphoneProducts.push(iphone13);
@@ -19,7 +22,7 @@ iphone12.id = 1;
 iphone12.url = "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2020/10/iphone-12-frandroid-2020.png";
 iphone12.name = "iphone12";
 iphone12.note = 9;
-iphone12.prix = 9;
+iphone12.prix = 799;
 iphone12.stock = 30;
 iphone12.details = "iphone 12 details";
 iphoneProducts.push(iphone12);
@@ -28,7 +31,7 @@ iphone11.id = 2;
 iphone11.url = "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2019/08/apple-iphone-11-frandroid-2019.png";
 iphone11.name = "iphone11";
 iphone11.note = 9;
-iphone11.prix = 9;
+iphone11.prix = 759;
 iphone11.stock = 30;
 iphone11.details = "iphone 11 details";
 iphoneProducts.push(iphone11);
@@ -37,7 +40,151 @@ iphone10.id = 3;
 iphone10.url = "https://c2.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2019/04/iphone-x-final.png";
 iphone10.name = "iphone10";
 iphone10.note = 9;
-iphone10.prix = 9;
+iphone10.prix = 599;
 iphone10.stock = 30;
 iphone10.details = "iphone 10 details";
 iphoneProducts.push(iphone10);
+window.iphoneProducts;
+
+// samsung products 
+var samsungProducts = [];
+var samsungS21 = {
+     id: 0,
+     url: "https://c2.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2020/12/samsung-galaxy-s21-frandroid-2021-leak.png",
+     name: "samsungS21",
+     note: 9,
+     prix: 699,
+     stock: 45,
+     details: "samsung details",
+};
+var samsungS10 = {
+     id: 1,
+     url: "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2019/04/samsung-galaxy-s10-2019-frandroid.png",
+     name: "samsungS10",
+     note: 9,
+     prix: 499,
+     stock: 55,
+     details: "samsung details",
+};
+var samsungA52 = {
+     id: 2,
+     url: "https://c1.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2021/08/samsung-galaxy-a52s-5g-frandroid-2021.png",
+     name: "samsungA52",
+     note: 8,
+     prix: 399,
+     stock: 25,
+     details: "samsung details",
+};
+var samsungA12 = {
+     id: 3,
+     url: "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2021/01/samsung-galaxy-a12-frandroid-2021.png",
+     name: "samsungA12",
+     note: 8,
+     prix: 320,
+     stock: 15,
+     details: "samsung details",
+};
+samsungProducts.push(samsungS21);
+samsungProducts.push(samsungS10);
+samsungProducts.push(samsungA52);
+samsungProducts.push(samsungA12);
+
+window.samsungProducts;
+
+// huawei products 
+var huaweiProducts = [];
+var huaweiP50 = {
+     id: 0,
+     url: "https://cdn-files.kimovil.com/default/0006/33/thumb_532239_default_big.jpeg",
+     name: "huaweiP50",
+     note: 9,
+     prix: 520,
+     stock: 15,
+     details: "huawei details",
+};
+var huaweiP40 = {
+     id: 1,
+     url: "https://c2.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2020/03/huawei-p40-pro-frandroid-2020.png",
+     name: "huaweiP40",
+     note: 9,
+     prix: 450,
+     stock: 15,
+     details: "huawei details",
+};
+var huaweiP30 = {
+     id: 2,
+     url: "https://c1.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2019/04/huawei-p30-2019-frandroid.png",
+     name: "huaweiP30",
+     note: 8,
+     prix: 400,
+     stock: 15,
+     details: "huawei details",
+};
+var huaweiP20 = {
+     id: 3,
+     url: "https://c1.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2019/04/huawei-p20.png",
+     name: "huaweiP20",
+     note: 9,
+     prix: 320,
+     stock: 15,
+     details: "huawei details",
+};
+huaweiProducts.push(huaweiP50);
+huaweiProducts.push(huaweiP40);
+huaweiProducts.push(huaweiP30);
+huaweiProducts.push(huaweiP20);
+
+window.huaweiProducts;
+
+// others product 
+
+var othersProducts = [
+     {
+          "id": 1,
+          "url": "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2020/10/iphone-12-frandroid-2020.png",
+          "name": "iphone12",
+          "note": 9,
+          "prix": 9,
+          "stock": 30,
+          "details": "iphone 12 details"
+     },
+     {
+          "id": 2,
+          "url": "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2019/08/apple-iphone-11-frandroid-2019.png",
+          "name": "iphone11",
+          "note": 9,
+          "prix": 9,
+          "stock": 30,
+          "details": "iphone 11 details"
+     },
+     {
+          "id": 0,
+          "url": "https://c2.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2020/12/samsung-galaxy-s21-frandroid-2021-leak.png",
+          "name": "samsungS21",
+          "note": 9,
+          "prix": 699,
+          "stock": 45,
+          "details": "samsung details"
+     },
+     {
+          "id": 1,
+          "url": "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2019/04/samsung-galaxy-s10-2019-frandroid.png",
+          "name": "samsungS10",
+          "note": 9,
+          "prix": 499,
+          "stock": 55,
+          "details": "samsung details"
+     },
+     {
+          "id": 3,
+          "url": "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2021/01/samsung-galaxy-a12-frandroid-2021.png",
+          "name": "samsungA12",
+          "note": 8,
+          "prix": 320,
+          "stock": 15,
+          "details": "samsung details"
+     }
+];
+
+window.othersProducts;
+
--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
+     $(".logo").on('click',()=>location.reload());
      $("#iphone").on('click',()=>renderProducts(iphoneProducts));
      $("#samsung").on('click', () => renderProducts(samsungProducts));
      $("#huawei").on('click', () => renderProducts(huaweiProducts));
      $("#others").on('click', () => renderProducts(othersProducts));
+     $("#cart").on('click', () => renderBuy(cart));
 
-     $(".main").html('');
-     $("#iphone").ready(renderProducts(iphoneProducts));
+     // delete this part to fix website
+     // $(".main").hide();
+     // $(".products").hide();
+     // $(".cart-container").ready(renderBuy(cart));
+
 });
 
 function each(coll, f) {
@@ -26,8 +31,19 @@ function map(array, f) {
      });
      return acc;
 }
+function reduce(array, f, acc) {
+     if (acc === undefined) {
+          acc = array[0];
+          array = array.slice(1);
+     }
+     each(array, function (element, i) {
+          acc = f(acc, element, i);
+     });
+     return acc;
+}
 
 function renderProduct(product){
+
      return $(`<div id=` + product.id + ` class="product">
                <div class="pic-product">
                     <img src="` + product.url + `">
@@ -40,7 +56,7 @@ function renderProduct(product){
                     <span>details... ` + product.details + `   </span>
                </div>
                <div class="buy-product">
-                    <button onclick=buy(` + product + `)> + cart</button>
+                    <button id="buy-btn" onclick="buy(`+ product.name +`)"> + cart</button>
                </div>
           </div>`);
 }
@@ -59,3 +75,110 @@ function renderProducts(products){
      });
 
 }
+
+function buy(p){
+     cart.push(p);
+     $("#count-cart").text(cart.length);
+     console.log(cart);
+}
+
+function renderBuy(cart){
+     var $cart = $(".cart-container").first();
+     $(".main").empty();
+     $(".products").empty();
+
+     $cart.empty();
+
+     if(cart.length === 0){
+          $cart.append("you don't have any products ...");
+     }else{
+          var $phones =""; 
+          each(cart, (phone) => {
+               $phones += "<tr><td>" + phone.name + "</td> <td>" + phone.prix + "</td></tr>"
+          });
+          var $total = 0;
+          each(cart, (phone) => $total += phone.prix);
+
+          var $content = `<div class="form">
+                              <label>full name</label><input id='full-name' type="text"> 
+                              <label>location</label><input id='location' type="text"> 
+                              <label>phone number</label><input id='phone' type="text"> 
+                              <label>pay method </label>
+                                   <select name="pay-methods" id="pay-methods">
+                                        <option value="cash">Cash </option>
+                                        <option value="Checks">Check </option>
+                                        <option value="debit-card">Debit cards </option>
+                                        <option value="credit-card">Credit cards </option>
+                                        <option value="mobile-pay">Mobile payments </option>
+                                        <option value="bank-transfert">Electronic bank transfers </option>
+                                   </select>
+                              <div class="buy-buttons">     
+                                   <button onclick='facture()'> pay </button>
+                                   <button> cancel </button>
+                              </div>     
+                         </div>
+                         <div class="cart-content">
+                              <table class="purchase">
+                                   <tr>
+                                        <th>model phones</th>
+                                        <th>phones prix</th>
+                                   </tr>
+                                   `+ $phones +`
+                                   <tr>
+                                        <td>total</td>
+                                        <td>`+$total+`</td>
+                                   </tr>
+                              </table>
+                         </div>
+               `;
+          $cart.append($content);
+     }
+}
+
+function send(){
+     $('#result').text('email sended ...');
+     $(".input").val('');
+}
+
+function facture(){
+     var fullName = $('#full-name').val();
+     var location = $('#location').val();
+     var phone    = $('#phone').val();
+     var payMethod = $('#pay-methods').find(':selected').val();
+
+     client = {fullName, location, phone, payMethod};
+     console.log(client);
+
+     var $phones = "";
+     each(cart, (phone) => {
+          $phones += "<tr><td>" + phone.name + "</td> <td>" + phone.prix + "</td></tr>"
+     });
+     var $total = 0;
+     each(cart, (phone) => $total += phone.prix);
+     var $facture = (`<div class="facture">
+                              <pre>
+                              <div class="client-details">
+                                   <span> client            : `+client.fullName+` </span> 
+                                   <span> location          : `+client.location+` </span> 
+                                   <span> phone number      : `+client.phone+` </span> 
+                                   <span> pay method        : `+client.payMethod+` </span> 
+                                   <span> total             : `+$total+` </span> 
+                              </div>     
+                              <table class="purchase">
+                                   <tr>
+                                        <th>model phones</th>
+                                        <th>phones prix</th>
+                                   </tr>
+                                   `+ $phones + `
+                                   <tr>
+                                        <td>total</td>
+                                        <td>`+ $total +`</td>
+                                   </tr>
+                              </table>
+                              </pre>
+                         </div>`);
+
+     $('.cart-container').html('');                    
+     $('.cart-container').append($facture);                    
+}
+
